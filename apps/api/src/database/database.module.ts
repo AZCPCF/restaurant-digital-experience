ea4cc5cc6876dd0175946d3c4c83e66e972cb8c3
Alisan <144158983+AZCPCF@users.noreply.github.com>
2026-09-03 05:12:10 +0330

    refactor(api): move shared abstractions to common

---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.getOrThrow<string>('DB_HOST'),
+        port: Number(config.getOrThrow<string>('DB_PORT')),
+        username: config.getOrThrow<string>('DB_USERNAME'),
+        password: config.getOrThrow<string>('DB_PASSWORD'),
+        database: config.getOrThrow<string>('DB_DATABASE'),
+
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
